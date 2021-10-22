16d4710674bfcc7cca9828f05617bf45d8ed3c6c
Mateo <mateo.loaiza1@correo.icesi.edu.co>
2021-10-22 11:48:59 -0500

    UserRegister and RestaurantRegister created

---
+++ app/src/main/java/com/example/foundeat/RestaurantRegister.java
@@ -0,0 +1,14 @@
+package com.example.foundeat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestaurantRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_register);
+    }
+}
