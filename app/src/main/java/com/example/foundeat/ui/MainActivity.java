09dd4da9e0b250a4984c3bc0b9fd02f2dcc4e9b3
Mateo <mateo.loaiza1@correo.icesi.edu.co>
2021-11-18 11:17:01 -0500

    Added sharedPreferences in MainActivity

--- app/src/main/java/com/example/foundeat/ui/MainActivity.java
+++ app/src/main/java/com/example/foundeat/ui/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import com.example.foundeat.R;
 import com.example.foundeat.model.Client;
 import com.example.foundeat.model.Restaurant;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.gson.Gson;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +24,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Cargar el usuario o restaurante
-        
+        client = loadClient();
+        restaurant = loadRestaurant();
+        if((client == null && restaurant == null) || FirebaseAuth.getInstance().getCurrentUser() == null || !FirebaseAuth.getInstance().getCurrentUser().isEmailVerified()){
+
+        }else{
+            if(client!= null){
+                Intent intent = new Intent(this,ClientHome.class);
+                intent.putExtra("client",client);
+                startActivity(intent);
+            }else if(restaurant!=null){
+                Intent intent = new Intent(this,RestaurantHome.class);
+                intent.putExtra("restaurant",restaurant);
+                startActivity(intent);
+            }
+        }
 
         clientBtn = findViewById(R.id.loginBtn);
         restaurantBtn = findViewById(R.id.registerBtn);
@@ -36,6 +52,23 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+    }
+
+    private Client loadClient(){
+        String json = getSharedPreferences("foundEat", MODE_PRIVATE).getString("client", "NO_USER");
+        if(json.equals("NO_USER")){
+            return null;
+        }else{
+            return new Gson().fromJson(json, Client.class);
+        }
+    }
+    private Restaurant loadRestaurant(){
+        String json = getSharedPreferences("foundEat", MODE_PRIVATE).getString("restaurant", "NO_USER");
+        if(json.equals("NO_USER")){
+            return null;
+        }else{
+            return new Gson().fromJson(json, Restaurant.class);
+        }
     }
 
 
