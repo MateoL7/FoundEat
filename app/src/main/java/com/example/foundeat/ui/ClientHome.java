2ac3b595bdbff475469a6b2a1655b2d6f59c08dc
Mateo <mateo.loaiza1@correo.icesi.edu.co>
2021-11-18 22:42:20 -0500

    Added a TV and Button to ClientHome and RestaurantHome to test the Logout

--- app/src/main/java/com/example/foundeat/ui/ClientHome.java
+++ app/src/main/java/com/example/foundeat/ui/ClientHome.java
@@ -2,15 +2,41 @@ package com.example.foundeat.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.foundeat.R;
+import com.example.foundeat.model.Client;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ClientHome extends AppCompatActivity {
+
+    private Client client;
+
+    private TextView nameET;
+    private Button clientLogoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_home);
+
+        client = (Client) getIntent().getExtras().get("client");
+        nameET = findViewById(R.id.nameTV);
+        nameET.setText(client.getName());
+        clientLogoutBtn = findViewById(R.id.clientLogoutBtn);
+        clientLogoutBtn.setOnClickListener(this::logout);
+    }
+
+    private void logout(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+        getSharedPreferences("foundEat", MODE_PRIVATE).edit().clear().apply();
+        FirebaseAuth.getInstance().signOut();
     }
 }
--- app/src/main/java/com/example/foundeat/ui/RestaurantHome.java
+++ app/src/main/java/com/example/foundeat/ui/RestaurantHome.java
@@ -2,15 +2,40 @@ package com.example.foundeat.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.foundeat.R;
+import com.example.foundeat.model.Client;
+import com.example.foundeat.model.Restaurant;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RestaurantHome extends AppCompatActivity {
+    private Restaurant restaurant;
+
+    private TextView nameET;
+    private Button resLogoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_restaurant_home);
+        setContentView(R.layout.activity_client_home);
+
+        restaurant = (Restaurant) getIntent().getExtras().get("restaurant");
+        nameET = findViewById(R.id.nameTV);
+        nameET.setText(restaurant.getName());
+        resLogoutBtn = findViewById(R.id.resLogoutBtn);
+        resLogoutBtn.setOnClickListener(this::logout);
+    }
+
+    private void logout(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+        getSharedPreferences("foundEat", MODE_PRIVATE).edit().clear().apply();
+        FirebaseAuth.getInstance().signOut();
     }
 }
