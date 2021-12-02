312d38d19f02fb7a94f1fc937ff24011b0e33c63
matheusvidartesandoval <math.vidarte@hotmail.com>
2021-12-02 14:07:01 -0500

    menu list done

--- app/src/main/java/com/example/foundeat/ui/restaurant/menuList/MenuListActivity.java
+++ app/src/main/java/com/example/foundeat/ui/restaurant/menuList/MenuListActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.example.foundeat.R;
@@ -23,7 +24,7 @@ import java.util.UUID;
 
 public class MenuListActivity extends AppCompatActivity {
 
-    private ImageButton addMenuItemBtn;
+    private Button addMenuItemBtn;
 
     private String newItemName,newItemPrice,newItemDescription, menuItemImage;
     private RecyclerView menuListRV;
