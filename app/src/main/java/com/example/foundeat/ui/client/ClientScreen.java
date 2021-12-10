532632b02fcdb886cd0bcc77c41c24bb8aee484e
matheusvidartesandoval <math.vidarte@hotmail.com>
2021-12-10 15:15:44 -0500

    design first client

--- app/src/main/java/com/example/foundeat/ui/client/ClientScreen.java
+++ app/src/main/java/com/example/foundeat/ui/client/ClientScreen.java
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.foundeat.R;
 import com.example.foundeat.ui.Login;
+import com.example.foundeat.ui.MainActivity;
 
 public class ClientScreen extends AppCompatActivity {
 
     private Button loginBtn, registerBtn;
+    private ImageButton goBackAll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,12 @@ public class ClientScreen extends AppCompatActivity {
 
         loginBtn = findViewById(R.id.loginBtn);
         registerBtn = findViewById(R.id.registerBtn);
+        goBackAll = findViewById(R.id.goBackAll);
+
+        goBackAll.setOnClickListener(v-> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
 
         loginBtn.setOnClickListener(v->{
             Intent intent = new Intent(this, Login.class);
@@ -30,6 +40,7 @@ public class ClientScreen extends AppCompatActivity {
             Intent intent = new Intent(this, ClientSignup.class);
             startActivity(intent);
         });
+
 
     }
 }
